67cd5ee80ee12358045187aedfef6ea67f728e0d
Mr. Je <165666975+Jerixdepunk@users.noreply.github.com>
2026-09-15 02:50:19 +0800

    Set permissions for tmh.ini file on saveConfig
    Added file permissions to the open call for tmh.ini.

--- jni/DrawMenu.h
+++ jni/DrawMenu.h
@@ -54,7 +54,7 @@ void loadConfig() {
     close(fd);
 }
 void saveConfig(){
-    int fd = open("/storage/emulated/0/Android/data/com.mobile.legends/tmh.ini", O_WRONLY | O_CREAT);
+    int fd = open("/storage/emulated/0/Android/data/com.mobile.legends/tmh.ini", O_WRONLY | O_CREAT, 0666);
     system("chmod 777 /storage/emulated/0/Android/data/com.mobile.legends/tmh.ini");
     write(fd, &Config , sizeof(Config));
     write(fd, &Aim, sizeof(Aim));
